0844d80ef3bf0e39fdfaf81e55f3e02a5454297c
Eduardo Zenebre <eduardo@zenebrelabs.com.br>
2021-11-25 21:21:42 -0300

    Array

---
+++ Array.py
@@ -0,0 +1,20 @@
+cadastro = [["Eduardo", 30], ["Erika", 33] ]
+
+a = ["a", "b", "c", "d", "e"]
+
+print(a)
+
+a.append("f")
+
+print(a)
+
+a.remove("a")
+
+print(a)
+
+a.pop(1)
+
+print(a)
+
+#for i in range(len(a)):
+#    print(a[i])
